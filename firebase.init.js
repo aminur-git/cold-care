51026dee9cf3a12ee5704ca3fa8b45135fca6bb2
aminur-git <aminur.4mi@gmail.com>
2025-05-07 13:24:29 +0600

    firebase-hosted

--- firebase.init.js
+++ firebase.init.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCrzX4lvQqwzx9ME8ckeaRtrh6noRW7sj8",
-  authDomain: "cold-care-36804.firebaseapp.com",
-  projectId: "cold-care-36804",
-  storageBucket: "cold-care-36804.firebasestorage.app",
-  messagingSenderId: "1050195758446",
-  appId: "1:1050195758446:web:27d007f8be95445aa6057f"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
